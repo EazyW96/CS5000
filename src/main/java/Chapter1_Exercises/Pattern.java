2382b29b87fe82df78e775246a14a1454c7a8b5c
Elliotte Wideman <ciaontop@gmail.com>
2024-01-10 17:04:49 -0500

    chapter 1 package made and pattern.java placed inside

--- src/main/java/Chapter1_Exercises/Pattern.java
+++ src/main/java/Chapter1_Exercises/Pattern.java
@@ -1,3 +1,5 @@
+package Chapter1_Exercises;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +10,9 @@
  * @author ciaon
  */
 public class Pattern {
-    
+   
+    public static void main(String[] args) {
+        
+        System.out.println("");
+    }
 }
