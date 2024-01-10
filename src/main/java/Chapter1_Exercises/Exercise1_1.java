152f21512161c67cabc2558a7fb83bb2e6b5ff3c
Elliotte Wideman <ciaontop@gmail.com>
2024-01-10 17:37:11 -0500

    Folder reorganized so programs can go in order

---
+++ src/main/java/Chapter1_Exercises/Exercise1_1.java
@@ -0,0 +1,17 @@
+// Introduction to Java programming and Data Structures, Daniel liang 12th edition.
+// Class Term:              Spring 2024
+// Name:                    Elliotte Wideman
+// IDE:                     netBeans
+/* (Display three messages) Write a program that displays Welcome to Java.
+    Welcome to Computer Science. and Programming is fun.*/
+
+
+
+
+public class Exercise1_1 {
+       public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun!");
+    }
+}
---
+++ src/main/java/Chapter1_Exercises/Exercises1_2.java
@@ -0,0 +1,16 @@
+
+// Introduction to Java programming and Data Structures, Daniel liang 12th edition.
+// Class Term:              Spring 2024
+// Name:                    Elliotte Wideman
+// IDE:                     netBeans
+/* (Display five messages) Write a program that displays Welcome to Java five times.*/
+
+public class Exercises1_2 {
+      public static void main(String[] args) {
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+    }
+}
