e2fd5cab4c9f921f7ba9ed386d76760b7a200aa6
Elliotte Wideman <ciaontop@gmail.com>
2024-01-09 23:01:34 -0500

    my first java repo

---
+++ src/main/java/WelcomeThreeMessages.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ciaon
+ */
+public class WelcomeThreeMessages {
+       public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun!");
+    }
+}
