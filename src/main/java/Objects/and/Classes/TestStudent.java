89cf76f36bae66086211368428ea5e36e4e0b01c
Elliotte Wideman <ciaontop@gmail.com>
2024-03-23 15:50:30 -0400

    Files added for chapters

---
+++ src/main/java/Objects/and/Classes/TestStudent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objects.and.Classes;
+
+/**
+ *
+ * @author ciaon
+ */
+public class TestStudent {
+    public static void main(String[] args) {
+    Student student = new Student(111223333, "Jamal");
+    System.out.println("Student was created on " + student.getDateCreated());
+    java.util.Date dateCreated = student.getDateCreated();
+    dateCreated.setTime(200000); // Now dateCreated field is changed!
+    System.out.println("Student's new time is " + student.getDateCreated());
+  }
+}
