6a05aa57129b2fa8715516a12bd6554207d88f6a
Elliotte Wideman <ciaontop@gmail.com>
2024-05-01 19:10:04 -0400

    GeometricObject.java Circle.java and Rectangle.java programs added

--- src/main/java/Chapt_13_AbstractClassesAndInterfaces/GeometricObject.java
+++ src/main/java/Chapt_13_AbstractClassesAndInterfaces/GeometricObject.java
@@ -23,5 +23,41 @@ public abstract class GeometricObject {
         this.filled = filled;
     }
     
+    /** Return color */
+    public String getColor() {
+        return color;
+    }
     
+    /** set a new color */
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    /** Return filled, Since filled is boolean, *
+     * the get method is named isFilled */
+    public boolean isFilled() {
+        return filled;
+    }
+    
+    /** Set a new filled */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    
+    /** Get datedCreated */
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+    
+    @Override
+    public String toString() {
+        return "created on " + dateCreated + "\ncolor: " + color +
+                " and filled: " + filled;
+    }
+    /** Abstract method getArea */
+    public abstract double getArea();
+    
+    /** Abstract method getPerimeter */
+    public abstract double getPerimeter();
+   
 }
