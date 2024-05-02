a441b8b722f85434ebd231f6f28e06a83e7a7e74
Elliotte Wideman <ciaontop@gmail.com>
2024-05-02 10:59:30 -0400

    new methods in TestEdible.java added

--- src/main/java/Chapt_13_AbstractClassesAndInterfaces/TestEdible.java
+++ src/main/java/Chapt_13_AbstractClassesAndInterfaces/TestEdible.java
@@ -9,46 +9,88 @@ package Chapt_13_AbstractClassesAndInterfaces;
  * @author ciaon
  */
 public class TestEdible {
+
     public static void main(String[] args) {
         Object[] objects = {new Tiger(), new Chicken(), new Apple()};
         for (int i = 0; i < objects.length; i++) {
             if (objects[i] instanceof Edible) {
-                System.out.println(((Edible)objects[i]).howToEat());
-            }
-            else {
-                System.out.println("Object is not edible");
+                System.out.println(((Edible) objects[i]).howToEat());
+
+                if (objects[i] instanceof Animal) {
+                    System.out.println(((Animal) objects[i]).sound());
+                }
             }
         }
     }
 }
 
+abstract class Animal {
 
-class Animal {
-    
+    private double weight;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * Return animal sound
+     */
+    public abstract String sound();
 }
 
 class Chicken extends Animal implements Edible {
+
+    @Override
     public String howToEat() {
         return "Chicken: Fry it";
     }
+
+    @Override
+    public String sound() {
+        return "Chicken: cock-a-doodle-doo";
+    }
+
 }
 
-class Tiger extends Animal{
-    
+class Tiger extends Animal implements Edible {
+
+    @Override
+    public String howToEat() {
+        return "Tiger: eats other tigers, or animals!";
+    }
+    @Override
+    public String sound()  {
+        return "Tiger: RRRRROOOOAARRR";
+    }
 }
 
 abstract class Fruit implements Edible {
-    
+// Data fields, constructors, and methods
 }
 
 class Apple extends Fruit {
+
+    @Override
     public String howToEat() {
         return "Apple: Make apple cider";
     }
 }
 
-class Orange extends Fruit{
+class Orange extends Fruit {
+
+    @Override
     public String howToEat() {
         return "Orange; Make orange juice";
     }
+    
+class Grape extends Fruit {
+    @Override
+    public String howToEat() {
+        return "Grapes with seeds are the best";
+    }
+}    
 }
